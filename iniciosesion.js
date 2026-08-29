47313d2743e82dcfb42bf1accdefd48662604d0f
daniel monsalve <gutmon.dm@gmail.com>
2026-08-28 20:57:36 -0500

    daniel modifico aqui

--- iniciosesion.js
+++ iniciosesion.js
@@ -1,7 +1,10 @@
- <script>
-    const usuarioCorrecto = "admin";
-    const contrasenaCorrecta = "1234";
+<script>
+   const usuarioCorrecto = "admin";
+   const contrasenaCorrecta = "1234";
 
-    let intentos = 0;
-    const maxIntentos = 3;
-    let sesionIniciada = false;
+   let intentos = 0;
+   const maxIntentos = 3;
+   let sesionIniciada = false;
+   if (usuario === usuarioCorrecto && contrasena === contrasenaCorrecta)
+   {alert("Bienvenido " + usuario);
+    sesionIniciada = true;      } 
