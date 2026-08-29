f9e0afea98563690c179b36ca56af4082ebf7676
Alexander Sanabria <alexandersanabria@MacBook-Air-de-Alexander.local>
2026-08-29 12:35:22 -0500

    commit de Lina

--- iniciosesion.js
+++ iniciosesion.js
@@ -5,6 +5,11 @@
    let intentos = 0;
    const maxIntentos = 3;
    let sesionIniciada = false;
+   
+   while (intentos < maxIntentos && sesionIniciada === false) {
+      let usuario = prompt("Ingrese su usuario:");
+      let contrasena = prompt("Ingrese su contraseña:");
+
    if (usuario === usuarioCorrecto && contrasena === contrasenaCorrecta)
    {alert("Bienvenido " + usuario);
     sesionIniciada = true;      } 
